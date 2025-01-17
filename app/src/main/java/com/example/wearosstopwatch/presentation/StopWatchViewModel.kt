16249881048532c41eef609e4e267a4d0b5015f4
Louie <dustyfingers01257@gmail.com>
2025-01-17 15:40:22 -0700

    fix build errors

--- app/src/main/java/com/example/wearosstopwatch/presentation/StopWatchViewModel.kt
+++ app/src/main/java/com/example/wearosstopwatch/presentation/StopWatchViewModel.kt
@@ -1,5 +1,7 @@
 @file:OptIn(ExperimentalCoroutinesApi::class)
 
+package com.example.wearosstopwatch.presentation
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -8,18 +10,17 @@ import kotlinx.coroutines.flow.*
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
-import com.example.wearosstopwatch.presentation.TimerState
-
 class StopWatchViewModel: ViewModel() {
+
     private val _elapsedTime = MutableStateFlow(0L)
     private val _timerState = MutableStateFlow(TimerState.RESET)
     val timerState = _timerState.asStateFlow()
 
-    private val formatter  = DateTimeFormatter.ofPattern("HH:mm:ss:SSS")
-
-    val stopWatchText = _elapsedTime.map {
-        millis -> LocalTime.ofNanoOfDay(millis * 1_000_000).format(formatter)
-    }
+    private val formatter = DateTimeFormatter.ofPattern("HH:mm:ss:SSS")
+    val stopWatchText = _elapsedTime
+        .map { millis ->
+            LocalTime.ofNanoOfDay(millis * 1_000_000).format(formatter)
+        }
         .stateIn(
             viewModelScope,
             SharingStarted.WhileSubscribed(5000),
@@ -27,37 +28,37 @@ class StopWatchViewModel: ViewModel() {
         )
 
     init {
-        _timerState.flapMapLatest { timerState ->
-            getTimerFlow(
-                isRunning = timerState == TimerState.RUNNING
-            )
-        }
+        _timerState
+            .flatMapLatest { timerState ->
+                getTimerFlow(
+                    isRunning = timerState == TimerState.RUNNING
+                )
+            }
             .onEach { timeDiff ->
                 _elapsedTime.update { it + timeDiff }
-
             }
             .launchIn(viewModelScope)
     }
 
-    fun resetTimer() {
-        _timerState.update{ TimerState.RESET }
-        _elapsedTime.update { 0L }
-    }
-
     fun toggleIsRunning() {
-        when (timerState.value) {
-            TimerState.RUNNING -> _timerState.update { TimerState.PAUSED },
+        when(timerState.value) {
+            TimerState.RUNNING -> _timerState.update { TimerState.PAUSED }
             TimerState.PAUSED,
             TimerState.RESET -> _timerState.update { TimerState.RUNNING }
         }
     }
 
+    fun resetTimer() {
+        _timerState.update { TimerState.RESET }
+        _elapsedTime.update { 0L }
+    }
+
     private fun getTimerFlow(isRunning: Boolean): Flow<Long> {
         return flow {
             var startMillis = System.currentTimeMillis()
-            while (isRunning) {
+            while(isRunning) {
                 val currentMillis = System.currentTimeMillis()
-                val (timeDiff) = if(currentMillis > startMillis) {
+                val timeDiff = if(currentMillis > startMillis) {
                     currentMillis - startMillis
                 } else 0L
                 emit(timeDiff)
